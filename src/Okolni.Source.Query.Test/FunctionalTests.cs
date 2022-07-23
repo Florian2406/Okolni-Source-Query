ad6f5c17dbcf32454af4ed3cd195f9cd4dbf82c9
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-23 19:35:07 -0400

    Add ASync Test, Update test server

--- src/Okolni.Source.Query.Test/FunctionalTests.cs
+++ src/Okolni.Source.Query.Test/FunctionalTests.cs
@@ -12,20 +12,40 @@ namespace Okolni.Source.Query.Test
         {
             IQueryConnection conn = new QueryConnection();
 
-            // Exampleserver: ARK VALGUERO Server
-            conn.Host = "89.163.146.91";
-            conn.Port = 27023;
+            // Random Server - Should find more stable one?
+            conn.Host = "185.239.211.62";
+            conn.Port = 39215;
 
             conn.Connect();
             var players1 = conn.GetPlayers();
             var players2 = conn.GetPlayers();
-
+            Assert.IsNotNull(players1);
+            Assert.IsNotNull(players2);
             //conn.Disconnect();
             // Console.WriteLine(players1.ToString());
             // Console.WriteLine(players2.ToString());
             // Console.WriteLine(players3.ToString());
             // Console.WriteLine(players4.ToString());
             // Console.WriteLine(players5.ToString());
+        }
+
+        [TestMethod]
+        public void AsyncQueryTest()
+        {
+            IQueryConnection conn = new QueryConnection();
+
+            // Random Server - Should find more stable one?
+            conn.Host = "185.239.211.62";
+            conn.Port = 39215;
+
+            conn.Connect();
+
+            var players = conn.GetPlayersAsync();
+            var info = conn.GetInfoAsync();
+            var rules = conn.GetRulesAsync();
+            Assert.IsNotNull(players);
+            Assert.IsNotNull(info);
+            Assert.IsNotNull(rules);
         }
     }
 }
